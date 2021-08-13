6cbc0ff6404e903ac894e914e35a9fb674415314
Deepashree <dvaideeswar@gmail.com>
2021-08-14 00:16:11 +0530

    initializing repository

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Paint Your World
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/deepa11/pen/WNjZWQE](https://codepen.io/deepa11/pen/WNjZWQE).
+
+
---
+++ src/script.js
@@ -0,0 +1,81 @@
+var paintcanvas = document.getElementById("canvas1");
+var context = paintcanvas.getContext("2d");
+var color = 'black';
+var radius = 50;
+paintcanvas.style.backgroundColor = '#FFFFFF'
+
+var isPainting = false;
+
+function setWidth(value) {
+ if (isNumeric(value)){
+   paintcanvas.width = value;
+ }
+}
+
+function clearCanvas () {
+  context.clearRect(0, 0, paintcanvas.width, paintcanvas.height);
+}
+
+function paintCircle (x, y) {
+    
+    context.beginPath();
+    
+    context.arc(x, y, radius, 0, Math.PI * 2, true);
+    context.fillStyle = color;
+    context.fill();
+}
+
+function isNumeric (value) {
+
+  return !isNaN(value);
+}
+
+function setHeight(value){
+  if(isNumeric(value)){
+    paintcanvas.height = value;
+  }
+}
+
+function startPaint(){
+  isPainting = true;
+}
+
+function endPaint(){
+  isPainting = false;
+}
+
+function doPaint(x, y){
+  if(isPainting == true){
+    paintCircle(x,y);
+  }
+}
+
+function changeColor(newColor){
+  color = newColor;
+}
+
+function resizeBrush(newSize){
+  radius = newSize;
+   document.getElementById("sizeOutput").value = newSize;
+}
+
+function background(newColor){
+  paintcanvas.style.backgroundColor = newColor;
+}
+
+function changeColoreraser(){
+  color = paintcanvas.style.backgroundColor;
+}
+function eraser(x, y) {
+    context.beginPath();
+    context.arc(x, y, radius, 0, Math.PI * 2, true);
+    context.fillStyle = color;
+    context.fill();
+}
+
+function erase(x, y){
+  if(isPainting == true){
+    eraser(x,y);
+  }
+}
+
